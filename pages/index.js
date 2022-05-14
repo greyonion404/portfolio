4b00867efad6e7178f30d099c2ff0549a44332e8
greyonion404 <105542223+greyonion404@users.noreply.github.com>
2022-05-14 15:38:30 +0600

    initial commit
    first commit for portfolio site. Blank next.js site.

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className={""}>
+      <Head>
+        <title>Portfolio</title>
+        <meta name="description" content="Portfolio site for Md. Maruf Bin Salim Bhuiyan" />
+      </Head>
+      </div>
+  )
+}
