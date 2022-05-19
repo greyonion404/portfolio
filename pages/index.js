ff61ec0a4583380268120d33477f80289c30e938
greyonion404 <105542223+greyonion404@users.noreply.github.com>
2022-05-19 14:54:07 +0600

    Added work and education to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Navbar from '../components/Navbar_Component/Navbar'
 import ProfileInformation from '../components/ProfileInformation_Component/ProfileInformation'
-import { Text } from '../components/Text_Component/Text'
+import WorkAndEducation from '../components/WorkAndEducation_Component/WorkAndEducation'
 export default function Home() {
   return (
     <>
       <Navbar/>
       <ProfileInformation/>
+      <WorkAndEducation/>
     </>
   )
 }
