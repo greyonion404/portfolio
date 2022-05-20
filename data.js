a8ca5e132ce974a6b2ee3cff6006607c80d92cb6
greyonion404 <105542223+greyonion404@users.noreply.github.com>
2022-05-20 21:32:22 +0600

    added study information to data.js

--- data.js
+++ data.js
@@ -19,9 +19,53 @@ const data = {
         WorkAndEducation: {
             prompt: "Work and Education",
             studiedAt: [
-                1, 2
+                {
+                    id: 1,
+                    institutionName: "North South University",
+                    keyInformation: "BSc in CSE",
+                    startTime: "20 Jan 2020",
+                    endTime: "present",
+                    websiteLink: null,
+                    description: "I am doing my BSc in computer science and engineering at North South University.",
+                },
+                {
+                    id: 2,
+                    institutionName: "Bangladesh Agriculture Univesity College",
+                    keyInformation: "Intermediate",
+                    startTime: "19 Jan 2017",
+                    endTime: "19 Jan 2019",
+                    websiteLink: null,
+                    description: "I have passed my HSC examination from Bangladesh Agriculture Univesity College, mymensingh.",
+                },
+                {
+                    id: 3,
+                    institutionName: "Mymensingh Zilla School",
+                    keyInformation: "High-school",
+                    startTime: "19 Jan 2010",
+                    endTime: "19 Nov 2018",
+                    websiteLink: null,
+                    description: "I have passed my SSC examination from Mymensingh Zilla School",
+                },
             ],
             workedAt: [
+                {
+                    id: 1,
+                    role: "junior engineer",
+                    workplaceName: "Amazon",
+                    startTime: "20 Jan 2021",
+                    endTime: "present",
+                    websiteLink: "https://www.aboutamazon.com/workplace",
+                    description: "I was offered and accepted a full-time offer with Amazon - a Software Development company in Georgia.",
+                },
+                {
+                    id: 2,
+                    role: "software developer",
+                    workplaceName: "Google",
+                    startTime: "12 Jan 2020",
+                    endTime: "20 Jan 2021",
+                    websiteLink: null,
+                    description: "I was offered and accepted a full-time offer with Google - a Software Development company in Georgia.",
+                },
 
             ],
         }
