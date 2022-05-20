2f24a787b7152d1c62787500de470ed928a713ec
greyonion404 <105542223+greyonion404@users.noreply.github.com>
2022-05-20 21:34:38 +0600

    completed blogs page functionality
    this page will show all the blog snippet, client side pagination used.

--- pages/blogs.js
+++ pages/blogs.js
@@ -1,8 +1,77 @@
+import { useState, useEffect } from 'react';
+
+import ReactPaginate from 'react-paginate';
+
 import Navbar from '../components/Navbar_Component/Navbar'
-export default function Home() {
+import BlogSnippets from '../components/BlogSnippets_Component/BlogSnippets';
+import { BlogSnippetsContainer } from '../components/BlogSnippets_Component/BlogSnippets.styles';
+
+
+
+
+
+
+export default function Blogs({ blogs }) {
+
+  const [currentpage, SetCurrentPage] = useState(0);
+  const [pageCount, setPageCount] = useState(0);
+  const ITEMS_PER_PAGE = 4;
+  useEffect(() => {
+    SetCurrentPage(0);
+    setPageCount(Math.ceil(blogs.length / ITEMS_PER_PAGE));
+  }, [blogs]);
+
+  const changePage = ({ selected }) => {
+    console.log(selected);
+    SetCurrentPage(selected);
+  }
   return (
-    <div>
-      <Navbar></Navbar>
-    </div>
+    <>
+      <Navbar />
+
+      <BlogSnippetsContainer>
+        {blogs && <BlogSnippets blogs={blogs.slice(currentpage * ITEMS_PER_PAGE, (currentpage + 1) * ITEMS_PER_PAGE)} />}
+      </BlogSnippetsContainer>
+
+      <ReactPaginate
+        previousLabel={"Previous"}
+        nextLabel={"Next"}
+        pageCount={pageCount}
+        onPageChange={changePage}
+        containerClassName={"PaginationContainer"}
+        pageLinkClassName={"PaginationLink"}
+        activeClassName={"ActivePaginationLink"}
+      />
+
+
+    </>
   )
 }
+
+
+
+
+// gets the props for the current homepage
+export const getServerSideProps = async pageContext => {
+
+  const sanityQuery = encodeURIComponent(`*[_type == "post"]{
+    title,
+    slug,
+    publishedAt,
+    mainImage,
+    authorName,
+    keywords,
+    description
+  }`);
+  const backendURL = `${process.env.NEXT_PUBLIC_SANITY_URL}${sanityQuery}`;
+  try {
+    const response = await fetch(backendURL).then(response => response.json());
+    const blogs = response.result;
+    if (!blogs) return { notFound: true };
+    blogs.sort((a, b) => a.publishedAt < b.publishedAt ? 1 : -1);
+    return { props: { blogs: blogs } };
+  }
+  catch (err) {
+    return { notFound: true };
+  }
+};
