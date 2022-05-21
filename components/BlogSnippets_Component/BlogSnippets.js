abce97b2c0affd1d4060622075d9bfe3d2c688a0
greyonion404 <105542223+greyonion404@users.noreply.github.com>
2022-05-21 17:03:23 +0600

    Added icons to blogSnippets

--- components/BlogSnippets_Component/BlogSnippets.js
+++ components/BlogSnippets_Component/BlogSnippets.js
@@ -6,7 +6,7 @@ import data from '../../data';
 import { BlogSnippet } from './BlogSnippets.styles';
 import { Text } from '../Text_Component/Text';
 import { useRouter } from 'next/router';
-
+import { FaReadme, FaPenFancy, FaCalendar } from 'react-icons/fa';
 
 
 
@@ -42,10 +42,18 @@ export default function BlogSnippets({ blogs }) {
                     <BlogSnippet key={article.slug.current} >
                         <Text active>{article.title}</Text>
                         <div>
-                            <img width={200} height={120} style={{marginBottom: "10px"}} src={urlForImage(article.mainImage)} />
+                            <img width={200} height={120} style={{ marginBottom: "10px" }} src={urlForImage(article.mainImage)} />
                         </div>
-                        <Text underlineOnHover onClick={() => { router.push(`/blogs/${article.slug.current}`) }}>Read This Blog</Text>
-                        <Text>{(article.authorName ? (article.authorName + " | ") : ("")) + formattedDate(article.publishedAt)}</Text>
+                        <Text underlineOnHover onClick={() => { router.push(`/blogs/${article.slug.current}`) }}>
+                            Read This Blog
+                            {<FaReadme style={{ marginLeft: "4px", fontSize: "90%" }} />}
+                        </Text>
+                        <Text>
+                            {article.authorName && <FaPenFancy style={{ marginRight: "4px", fontSize: "60%" }} />}
+                            {article.authorName && article.authorName}
+                            {<FaCalendar style={{ marginLeft: "4px", marginRight: "4px", fontSize: "60%" }} />}
+                            {formattedDate(article.publishedAt)}
+                        </Text>
                     </BlogSnippet>
                 )
             }
