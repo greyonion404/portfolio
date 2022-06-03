c7a333f88a31cb51e4d9cd6818fd2052b7f944c2
greyonion404 <105542223+greyonion404@users.noreply.github.com>
2022-06-03 13:42:15 +0600

    added nextProgress component to the site

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
+import NextNProgress from 'nextjs-progressbar';
 import '../styles/globals.css'
+import { useState } from 'react'
+import data from '../data';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [loading, setLoading] = useState(false)
+  return (
+    <>
+      <NextNProgress
+        options={{ easing: "ease", speed: 500 }}
+        color={data.styles.color.secondary}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
