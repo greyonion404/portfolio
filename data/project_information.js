4930b2bc56d130d6dd5430f53a3a5323c6e30166
greyonion404 <105542223+greyonion404@users.noreply.github.com>
2022-06-15 22:48:23 +0600

    updated

--- data/project_information.js
+++ data/project_information.js
@@ -3,6 +3,10 @@ const projects = [
         title: 'github',
         slug: 'github',
         mainImage: 'https://picsum.photos/200',
+        images: [
+            'https://drive.google.com/uc?id=1Vcg_vjbJXtsQOBefBSj64ABCSFrZqy0B',
+            'https://c.tenor.com/Q40RmcF-IkkAAAAi/kiss.gif'
+        ],
         shortDescription: "this is a demo short description about a demo project this is a demo short description about a demo project",
         description: "this is a demo description about a demo project\n this is a demo description about a demo project this is a demo description about a demo project this is a demo description about a demo project this is a demo description about a demo project",
     }
--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -3,6 +3,8 @@ import Navbar from '../../components/Navbar_Component/Navbar'
 import { ProjectContainer } from '../../components/ProjectSnippets_Component/ProjectSnippets.styles';
 import { Text } from '../../components/Text_Component/Text';
 import data from '../../data';
+import ImageViewer from "react-simple-image-viewer";
+import { AiOutlineExpandAlt } from 'react-icons/ai';
 
 
 function SpecifiedText({ size, content, textAlign, width, paddingLeft }) {
@@ -20,11 +22,94 @@ function SpecifiedText({ size, content, textAlign, width, paddingLeft }) {
 
 }
 
+function ModalOpenText({ size, content, textAlign, width, paddingLeft, onClick }) {
+
+    let style = {
+        paddingLeft: paddingLeft,
+        width: width,
+        textAlign: textAlign,
+    }
+    return (
+        <Text size={size} style={style}>
+            {content}
+        </ Text>
+    );
+
+}
+
+
+function createImageRenderArray(images) {
+    let imageRendereArray = [];
+    for (let i = 0; i < imageList.length; i++) {
+        imageRendereArray.push({ source: images[i] });
+    }
+    return imageRendereArray;
+}
+
+
+function ImageViewerUI({ images }) {
+
+    const [currentImage, setCurrentImage] = useState(0);
+    const [isViewerOpen, setIsViewerOpen] = useState(false);
+
+
+
+    const openImageViewer = (index) => {
+        setCurrentImage(index);
+        setIsViewerOpen(true);
+    };
+
+    const closeImageViewer = () => {
+        setCurrentImage(0);
+        setIsViewerOpen(false);
+    };
+
+    if (images && images.length !== 0) {
+
+        let content = <>
+            {"See all images that showcases this project. "}
+            <AiOutlineExpandAlt color={data.styles.color.text.lighter} onClick={() => { openImageViewer(0) }} />
+        </>
+
+        return (
+            <>
+
+
+                <SpecifiedText
+                    content={content}
+                    size={2} width={"100%"} textAlign="left" paddingLeft={"5%"}
+
+                />
+
+                <img
+                    style={{ width: "50%", marginLeft: "5%", marginTop: "20px", marginBottom: "10px" }}
+                    src={images[0]}
+                    onClick={() => { openImageViewer(0) }}
+                />
+
+
+
+                {isViewerOpen && (
+                    <ImageViewer
+                        src={images}
+                        currentIndex={currentImage}
+                        onClose={closeImageViewer}
+                        disableScroll={false}
+                        backgroundStyle={{ ibackgroundColor: "rgba(0,0,0,0.9)" }}
+                        closeOnClickOutside={true}
+                    />
+                )}
+            </>
+        )
+    }
+}
 
 
 export default function Project({ project }) {
 
-    const { title, slug, mainImage, description, shortDescription } = project;
+    const { title, images, mainImage, description } = project;
+
+
 
     return (
         <>
@@ -34,6 +119,7 @@ export default function Project({ project }) {
                 <img style={{ width: "90%", aspectRatio: "100/45", margin: "auto", marginBottom: "10px" }} src={mainImage} />
                 <SpecifiedText content={"Description : "} size={2} width={"90%"} textAlign="left" paddingLeft={"5%"} />
                 <SpecifiedText content={description} size={1} width={"90%"} textAlign="left" paddingLeft={"5%"} />
+                <ImageViewerUI images={images} />
             </ProjectContainer>
 
 
