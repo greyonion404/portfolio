0d8f17978e7b224b0b4ce046e04e375b2be3d60f
greyonion404 <twitterpicker111@gmail.com>
2022-10-03 14:44:35 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import Navbar from '../components/Navbar_Component/Navbar'
 import ProfileInformation from '../components/ProfileInformation_Component/ProfileInformation'
 import WorkAndEducation from '../components/WorkAndEducation_Component/WorkAndEducation'
+import Head from 'next/head'
 export default function Home() {
   return (
     <>
-      <Navbar/>
-      <ProfileInformation/>
-      <WorkAndEducation/>
+      <Head>
+        <meta name="a.validate.02" content="YifPncGjT06aI8JchBDqLQujMBZagpCCGOpW" />
+      </Head>
+      <Navbar />
+      <ProfileInformation />
+      <WorkAndEducation />
     </>
   )
 }
